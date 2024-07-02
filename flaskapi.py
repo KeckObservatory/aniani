a18e2985fabb0a141cdc8f2236d3b5f711a954aa
avaDLG <100103395+avaDLG@users.noreply.github.com>
2024-07-02 11:08:11 -1000

    moved db functions to a new file to reuse them

--- flaskapi.py
+++ flaskapi.py
@@ -1,8 +1,31 @@
-from flask import Flask, jsonify, request
+from flask import Flask
+from flask_mysqldb import MySQL
 from os.path import isfile
 import configparser
 
 app = Flask(__name__)
+
+#---------------------------------------
+# ANIANI Class
+#---------------------------------------
+class aniani:
+
+    def __init__():
+
+        # read the config file
+        config = configparser.ConfigParser()
+        config.read('config.live.ini')
+
+        # grab the information from the config file
+        db_config = {
+            'host': config.get('mysql', 'host'),
+            'port': config.getint('mysql', 'port'),
+            'admin_username': config.get('mysql', 'username'),
+            'admin_password': config.get('mysql', 'password'),
+            'database': config.get('mysql', 'database')
+        }
+            
+
 
 #---------------------------------------
 # ANIANI API Routes
@@ -17,11 +40,11 @@ def home():
     return {"status":"ERROR", "message":"something went wrong"}
 
 
-
 if __name__ == "__main__":
+
     api = "aniani"
 
-     # Parse config file 
+    # read config
     config = configparser.ConfigParser()
     config.read('config.live.ini')
 
--- startup.py
+++ startup.py
@@ -3,54 +3,7 @@ import configparser
 import mysql.connector
 from mysql.connector import Error
 import pandas
-
-def read_config(config_file):
-    """
-    Function to read the config file.
-
-    :param config_file: path to the config file
-    :type config_file: str
-    :return: a dict with all extracted config attributes
-    :rtype: dict
-    """
-
-    # read the config file
-    config = configparser.ConfigParser()
-    config.read(config_file)
-
-    # grab the information from the config file
-    db_config = {
-        'host': config.get('mysql', 'host'),
-        'port': config.getint('mysql', 'port'),
-        'admin_username': config.get('mysql', 'username'),
-        'admin_password': config.get('mysql', 'password'),
-        'database': config.get('mysql', 'database')
-    }
-
-    # return the dict
-    return db_config
-
-
-def create_db_connection(db_config):
-    """
-    Function to create a mysql/python connection to access db
-
-    :param db_config: dict with all the config attributes from read_config()
-    :type db_config: dict
-    :return: the connection between python and the database
-    :rtype: connection object?
-    """
-
-    # create the connection to db 
-    connection = mysql.connector.connect(
-        host=db_config['host'],
-        port=db_config['port'],
-        user=db_config['admin_username'],
-        password=db_config['admin_password']
-    )
-
-    # returning the connection
-    return connection
+from aniani_functions import *
 
 
 def run_sql_script(connection, script_path):
@@ -140,7 +93,7 @@ def populate_db(db_config, connection, files):
 
 if __name__ == "__main__":
 
-    db_config = read_config('config.live.ini')
+    db_config = read_db_config('config.live.ini')
 
     connection = create_db_connection(db_config)
 
